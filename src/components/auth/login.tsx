e9b6d24d171d4ea86bfc73b65d4efb62a45e08d1
Ethan Benabou <ethanbnb@gmail.com>
2022-02-16 13:48:04 -0500

    WIP

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,3 +1,15 @@
-const Login = (): JSX.Element => <div>Login</div>;
+import Image from "next/image";
+import kitty from "../../../public/kitty.jpeg";
+
+const Login = (): JSX.Element => {
+  console.log(kitty);
+
+  return (
+    <div>
+      <Image src={kitty} alt="kitty" />
+      <img src={kitty.src} alt="asd" />
+    </div>
+  );
+};
 
 export { Login };
