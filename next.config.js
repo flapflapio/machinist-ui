c5840c4795e0fd7e9036aaf223ed1ae5e243b29c
Ethan Benabou <ethanbnb@gmail.com>
2022-02-16 13:39:48 -0500

    Support for importing images
    Upgraded next and typescript to support importing images directly into
    components.

--- next.config.js
+++ next.config.js
@@ -2,17 +2,13 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  // basePath: process.env.NODE_ENV === "production" ? "/template" : "",
-  // assetPrefix: process.env.NODE_ENV === "production" ? "/template/" : "/",
 
-  basePath: process.env.ASSET_PREFIX || "",
-  assetPrefix: `${process.env.ASSET_PREFIX || ""}/`,
+  // Use below if you need to serve assets from somewhere else
+  // basePath: process.env.NODE_ENV === "production" ? "" : "",
+  // assetPrefix: process.env.NODE_ENV === "production" ? "" : "",
+  // basePath: process.env.ASSET_PREFIX || "",
+  // assetPrefix: `${process.env.ASSET_PREFIX || ""}/`,
 
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/i,
-      type: "asset/resource",
-    });
-    return config;
-  },
+  // Use below to configure webpack
+  webpack: (config) => config,
 };
