b5775ed4f36dea92d4cb54b4c886bc489bb10a1f
Ethan Benabou <ethanbnb@gmail.com>
2022-03-10 16:27:03 -0500

    WIP: initial amplify configuration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "machinistuid535972aDefineAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "machinistuid535972aCreateAuthChallenge": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "machinistuid535972aVerifyAuthChallengeResponse": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "machinistui90c96205": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,39 @@
+import { useAuthenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import { CognitoUserAmplify } from "@aws-amplify/ui-react/node_modules/@aws-amplify/ui";
+import "@aws-amplify/ui-react/styles.css";
 import "antd/dist/antd.css";
-import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import type { AppProps as NextAppProps } from "next/app";
 import Head from "next/head";
+import { createContext, useMemo } from "react";
 import styled, { ThemeProvider } from "styled-components";
+import awsconfig from "../src/aws-exports";
 import { theme } from "../src/util/styles";
 import "./app.global.css";
+
+Amplify.configure(awsconfig);
 
 const Footer = styled.footer`
   display: none;
 `;
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+type AuthProps = {
+  signOut?: ReturnType<typeof useAuthenticator>["signOut"];
+  user?: CognitoUserAmplify;
+};
+
+type AppProps = NextAppProps & AuthProps;
+
+const AuthContext = createContext<AuthProps>({});
+
+const App = ({
+  signOut,
+  user,
+  Component,
+  pageProps,
+}: AppProps): JSX.Element => {
+  const authPackage = useMemo(() => ({ user, signOut }), [user, signOut]);
+
   return (
     <>
       <Head>
@@ -19,10 +43,14 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
         />
       </Head>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <AuthContext.Provider value={authPackage}>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AuthContext.Provider>
       <Footer>Machinist</Footer>
     </>
   );
-}
+};
+
+export default withAuthenticator(App);
