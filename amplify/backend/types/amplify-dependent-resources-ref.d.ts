8343ac394b31a112f3b67a4aeb93f6874caca829
Ethan Benabou <ethanbnb@gmail.com>
2022-03-12 19:45:47 -0500

    Demo for how to do file storage
    Closes: FLAP-183

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "machinistuis3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
