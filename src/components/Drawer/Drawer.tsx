1d062e79420b5d46565fc66a46e8e97e942350d9
Amneet  Singh <amneet.s.270@gmail.com>
2022-03-29 14:10:33 -0400

    Got a basic demo for file storage, changing from js to tsx

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import {
+  Drawer as Drawerant,
+  Button,
+  Space,
+  Form,
+  Input,
+  Checkbox,
+} from "antd";
+import { DownloadOutlined, SettingOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
+import styled from "styled-components";
+
+const FormRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const BigAvatar = styled(Avatar)`
+  width: 10em;
+  height: 10em;
+`;
+
+const Drawer = () => {
+  const [visible, setVisible] = useState(false);
+  const [size, setSize] = useState<string>();
+
+  const showDefaultDrawer = () => {
+    setSize("default");
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <Space>
+        <Button
+          type="primary"
+          shape="circle"
+          icon={<SettingOutlined />}
+          size={"large"}
+          onClick={showDefaultDrawer}
+        ></Button>
+        <div onClick={showDefaultDrawer}>
+          <Avatar
+            size="large"
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            // onClick={showDefaultDrawer}
+          />
+        </div>
+      </Space>
+      <Drawerant
+        title={`User accounts`}
+        placement="right"
+        //size={size}
+        onClose={onClose}
+        visible={visible}
+        extra={
+          <Space>
+            <Avatar
+              size="large"
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            />
+          </Space>
+        }
+      >
+        <div></div>
+        <FormRoot>
+          <BigAvatar
+            size="large"
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+          />
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            autoComplete="off"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="email"
+              name="email"
+              rules={[{ required: true, message: "Please input your email!" }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Modify
+              </Button>
+            </Form.Item>
+          </Form>
+          <Button
+            type="primary"
+            shape="round"
+            size={"large"}
+            style={{ marginTop: "auto" }}
+          >
+            Sign out
+          </Button>
+        </FormRoot>
+      </Drawerant>
+    </>
+  );
+};
+
+export default Drawer;
